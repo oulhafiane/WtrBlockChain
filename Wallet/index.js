150166c705694532a32c1a004c8ddebf20997616
Zakariaa Oulhafiane <zakariaa.oulhafiane@gmail.com>
2019-07-26 09:33:08 +0100

    Removed wallet and keept the client

--- Wallet/index.js
+++
@@ -1,7 +0,0 @@
-const { TransactionProcessor } = require('sawtooth-sdk/processor');
-const WalletHandler = require('./srcs/wallet_handler');
-
-const transactionProcessor = new TransactionProcessor('tcp://127.0.0.1:4004');
-
-transactionProcessor.addHandler(new WalletHandler());
-transactionProcessor.start();
